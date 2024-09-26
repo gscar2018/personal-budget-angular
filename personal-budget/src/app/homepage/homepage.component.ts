c23fdd2ddbd1cbc95b7a37d8c6e5cb06305bc032
gscar2018 <gscardo@ymail.com>
2024-09-25 23:56:14 -0400

    added data service and d3chart

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,13 +1,15 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Chart , registerables} from 'chart.js';
+import { Chart, registerables } from 'chart.js';
+import { DataService } from '../data.service';
+import * as d3 from 'd3';
+
 @Component({
   selector: 'pb-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements AfterViewInit {
-  
+
   public dataSource = {
     datasets: [
       {
@@ -23,27 +25,70 @@ export class HomepageComponent implements AfterViewInit {
     labels: [] as string[]
   };
 
-  constructor(private http: HttpClient) {
+  constructor(private dataService: DataService) {
     Chart.register(...registerables);
-   }
+  }
 
   ngAfterViewInit(): void {
-    this.http.get('http://localhost:3000/budget')
-    .subscribe((res: any) => {
+    this.dataService.getData().subscribe((res: any) => {
       for (var i = 0; i < res.myBudget.length; i++) {
         this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
         this.dataSource.labels[i] = res.myBudget[i].title;
-
-    }
-    this.createChart();
+      }
+      this.createChart();
+      this.createD3Chart(res.myBudget);
     });
   }
+
   createChart() {
     var ctx = document.getElementById('myChart') as HTMLCanvasElement;
     var myPieChart = new Chart(ctx, {
       type: 'pie',
       data: this.dataSource  // Pass the updated dataSource
     });
-}
+  }
 
+  createD3Chart(data: { title: string; budget: number }[]): void {
+    const width = 450;
+    const height = 450;
+    const margin = 40;
+
+    const radius = Math.min(width, height) / 2 - margin;
+
+    const svg = d3.select("#d3chart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2},${height / 2})`);
+
+    const color = d3.scaleOrdinal()
+      .domain(data.map(d => d.title))
+      .range(d3.schemeCategory10);
+
+    const pie = d3.pie<{ title: string; budget: number }>()
+      .value(d => d.budget);
+
+    const arc = d3.arc<d3.PieArcDatum<{ title: string; budget: number }>>()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    const arcs = svg.selectAll("arc")
+      .data(pie(data))
+      .enter()
+      .append("g")
+      .attr("class", "arc");
+
+    arcs.append("path")
+      .attr("d", arc)
+      .attr("fill", d => color(d.data.title) as string);
+
+    arcs.append("text")
+      .attr("transform", d => `translate(${arc.centroid(d)})`)
+      .attr("text-anchor", "middle")
+      .text(d => d.data.title);
+
+    // Debugging: Log the data to ensure it's correct
+    console.log('D3 Data:', data);
+  }
 }
